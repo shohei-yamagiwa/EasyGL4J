544284314a95ced7be5b1295f50e74aef9fcc860
Hazuki <77273686+mano-hazuki@users.noreply.github.com>
2024-11-08 13:07:44 +0900

    Removed some test codes.

--- src/main/java/dev/shoheiyamagiwa/example/Main.java
+++ src/main/java/dev/shoheiyamagiwa/example/Main.java
@@ -1,17 +1,12 @@
 package dev.shoheiyamagiwa.example;
 
-import dev.shoheiyamagiwa.easygl4j.opengl.GLButton;
-import dev.shoheiyamagiwa.easygl4j.opengl.GLTextField;
 import dev.shoheiyamagiwa.easygl4j.opengl.GLWindow;
 
 public class Main {
 
     public static void main(String[] args) {
-        GLWindow window = new GLWindow("Example Window", 1920, 1080);
+        GLWindow window = new GLWindow("Example Window", 1600, 900);
         window.setVisible(true);
-        window.setFullscreen(true);
-        window.add(new GLButton());
-        window.add(new GLTextField());
         window.run();
     }
 }
