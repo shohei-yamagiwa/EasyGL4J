8acc844a028a5ab4a85292613722a33c6dbcce7b
Hazuki <77273686+mano-hazuki@users.noreply.github.com>
2024-11-08 09:26:09 +0900

    Created text field component class.

---
+++ src/main/java/dev/shoheiyamagiwa/easygl4j/opengl/GLTextField.java
@@ -0,0 +1,14 @@
+package dev.shoheiyamagiwa.easygl4j.opengl;
+
+import dev.shoheiyamagiwa.easygl4j.Container;
+
+public class GLTextField extends GLComponent {
+    /**
+     * Creates a new text field with given container as parent
+     *
+     * @param parent The parent container.
+     */
+    protected GLTextField(Container parent) {
+        super(parent);
+    }
+}
